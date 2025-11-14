e7101aa5ca77cd258e407c7fa27fa9707b1987dc
Duskafka <rlarlgus0206@naver.com>
2025-11-14 21:40:38 +0900

    update(#37): reissueTokens 메소드를 삭제하고 주석 추가
    - reissueTokens와 refreshToken의 메소드의 역할이 중복되는 것을 확인하고 한 가지 메소드만 남김.
    - Javadocs로 문서화.

--- src/main/java/com/aetheri/application/port/in/token/RefreshTokenUseCase.java
+++ src/main/java/com/aetheri/application/port/in/token/RefreshTokenUseCase.java
@@ -3,7 +3,14 @@ package com.aetheri.application.port.in.token;
 import com.aetheri.application.result.jwt.TokenResult;
 import reactor.core.publisher.Mono;
 
+/**
+ * 리프래쉬 토큰을 재발급 해주는 유즈케이스입니다.
+ * */
 public interface RefreshTokenUseCase {
-    Mono<TokenResult> refreshToken(Long runnerId);
-    Mono<TokenResult> reissueTokens(String refreshToken);
+
+    /**
+     * @param refreshToken 갱신이 요청된 리프레시 토큰입니다.
+     * @return 새로운 토큰 정보를 담은 {@code TokenResult}를 발행하는 {@code Mono}입니다.
+     * */
+    Mono<TokenResult> refreshToken(String refreshToken);
 }
